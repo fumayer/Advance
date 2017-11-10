6783e27708e38cfa020b70daf89c1aaecc604bfb
sunjie <sunjie@rgbvr.com>
2017-11-10 20:29:56 +0800

    代码中设置shape选择器

--- pratice/MyApplication/app/src/main/java/com/example/dzh/myapplication/TestShapeActivity.java
+++ pratice/MyApplication/app/src/main/java/com/example/dzh/myapplication/TestShapeActivity.java
@@ -2,12 +2,16 @@ package com.example.dzh.myapplication;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,22 +46,22 @@ public class TestShapeActivity extends AppCompatActivity {
 
     public void go2(View view) {
         Toast.makeText(this, "触发go2", Toast.LENGTH_SHORT).show();
-        GradientDrawable myGrad = (GradientDrawable)mTv.getBackground();
+        GradientDrawable myGrad = (GradientDrawable) mTv.getBackground();
         myGrad.setColor(getResources().getColor(R.color.red));
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     public void go3(View view) {
         Toast.makeText(this, "触发go3", Toast.LENGTH_SHORT).show();
-        GradientDrawable myGrad = (GradientDrawable)mTv.getBackground();
+        GradientDrawable myGrad = (GradientDrawable) mTv.getBackground();
 //         生成的是混合色
-        myGrad.setColors(new int[]{R.color.blue,R.color.red});
+        myGrad.setColors(new int[]{R.color.blue, R.color.red});
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)   /* 16 */
     public void go4(View view) {
         Toast.makeText(this, "触发go4", Toast.LENGTH_SHORT).show();
-        int colors[] = { 0xff255779 , 0xff3e7492, 0xffa6c0cd };//分别为开始颜色，中间夜色，结束颜色
+        int colors[] = {0xff255779, 0xff3e7492, 0xffa6c0cd};//分别为开始颜色，中间夜色，结束颜色
 
         GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colors);
 
@@ -71,4 +75,51 @@ public class TestShapeActivity extends AppCompatActivity {
         gd.setStroke(strokeWidth, strokeColor);
         mTv.setBackground(gd);
     }
-}
+
+
+    public void go5(View view) {
+        Toast.makeText(this, "触发go5", Toast.LENGTH_SHORT).show();
+
+        GradientDrawable normalDraw = getDrawable(R.color.blue, 100f, 0, -1);
+        GradientDrawable pressDraw = getDrawable(R.color.yellow, 500f, 0, -1);
+        StateListDrawable selector = getSelector(normalDraw, pressDraw);
+        Button bt = (Button) view;
+        bt.setBackgroundDrawable(selector);
+
+    }
+
+    public void go6(View view) {
+        Toast.makeText(this, "触发go6", Toast.LENGTH_SHORT).show();
+    }
+
+    public void go7(View view) {
+        Toast.makeText(this, "触发go7", Toast.LENGTH_SHORT).show();
+    }
+
+    public void go8(View view) {
+        Toast.makeText(this, "触发go8", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public GradientDrawable getDrawable(@ColorRes int rgb, float radius, int stroke, @ColorRes int strokeColor) {
+        GradientDrawable gradientDrawable = new GradientDrawable();
+        gradientDrawable.setColor(getResources().getColor(rgb));//设置颜色
+//        gradientDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT);//设置显示的样式
+        gradientDrawable.setGradientType(GradientDrawable.SWEEP_GRADIENT);//设置显示的样式
+        gradientDrawable.setCornerRadius(radius);//设置圆角的半径
+        if (stroke != 0 && strokeColor != -1) {
+            gradientDrawable.setStroke(stroke, getResources().getColor(strokeColor));//描边
+        }
+        return gradientDrawable;
+    }
+
+
+    public StateListDrawable getSelector(Drawable normalDrawable, Drawable pressDrawable) {
+        StateListDrawable stateListDrawable = new StateListDrawable();
+//给当前的颜色选择器添加选中图片指向状态，未选中图片指向状态
+        stateListDrawable.addState(new int[]{android.R.attr.state_enabled, android.R.attr.state_pressed}, pressDrawable);
+//设置默认状态
+        stateListDrawable.addState(new int[]{}, normalDrawable);
+        return stateListDrawable;
+    }
+            }
