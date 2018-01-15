e654b1815915300b042b53f55a90ee8ead82eac9
SunJie <sunjie@sunjiedeMacBook-Pro.local>
2018-01-15 14:20:17 +0800

    添加SeekBar

---
+++ pratice/MyApplication/app2/src/main/java/com/example/sj/app2/seekbar/SeekBarActivity.java
@@ -0,0 +1,15 @@
+package com.example.sj.app2.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sj.app2.R;
+
+public class SeekBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seek_bar);
+    }
+}
