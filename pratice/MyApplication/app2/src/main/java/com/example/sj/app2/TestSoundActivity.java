bca810f252b6ee9188cab0d472bd1a12a1703b8d
SunJie <sunjie@SJ-MBP.local>
2018-04-11 16:51:54 +0800

    使用soundplayUtils

--- pratice/MyApplication/app2/src/main/java/com/example/sj/app2/TestSoundActivity.java
+++ pratice/MyApplication/app2/src/main/java/com/example/sj/app2/TestSoundActivity.java
@@ -54,12 +54,11 @@ public class TestSoundActivity extends AppCompatActivity {
         Toast.makeText(this, "触发go3", Toast.LENGTH_SHORT).show();
         view.setSoundEffectsEnabled(true);
         view.playSoundEffect(SoundEffectConstants.CLICK);
+        SoundPlayUtils.init(this);
     }
 
     public void go4(View view) {
-        Toast.makeText(this, "触发go4", Toast.LENGTH_SHORT).show();
-//        view.performHapticFeedback(HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
-//        view.playSoundEffect(SoundEffectConstants.NAVIGATION_UP);
+        SoundPlayUtils.play(SoundPlayUtils.MusicType.OPER.getValue());
 
     }
 
@@ -69,7 +68,7 @@ public class TestSoundActivity extends AppCompatActivity {
                 0.1f,   //左耳道音量【0~1】
                 0.5f,   //右耳道音量【0~1】
                 0,     //播放优先级【0表示最低优先级】
-                0 ,     //循环模式【0表示循环一次，-1表示一直循环，其他表示数字+1表示当前数字对应的循环次数】
+                0,     //循环模式【0表示循环一次，-1表示一直循环，其他表示数字+1表示当前数字对应的循环次数】
                 1     //播放速度【1是正常，范围从0~2】
         );
     }
