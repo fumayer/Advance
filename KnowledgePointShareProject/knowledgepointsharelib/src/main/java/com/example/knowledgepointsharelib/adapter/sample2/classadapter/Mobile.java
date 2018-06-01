e1a4edc531b074b7ab10d38f989d422c407121ca
sunjie <sunjie@rgbvr.com>
2018-06-01 12:02:01 +0800

    添加适配器模式之类适配器

---
+++ KnowledgePointShareProject/knowledgepointsharelib/src/main/java/com/example/knowledgepointsharelib/adapter/sample2/classadapter/Mobile.java
@@ -0,0 +1,21 @@
+package com.example.knowledgepointsharelib.adapter.sample2.classadapter;
+
+/**
+ * Created by sunjie on 2018/5/31.
+ * Client类：手机 .需要5V电压
+ */
+public class Mobile {
+    /**
+     * 充电方法
+     *
+     * @param voltage5
+     */
+    public void charging(Voltage5 voltage5) {
+        System.out.println("插上充电器充电");
+        if (voltage5.output5V() == 5) {
+            System.out.println("电压刚刚好5V，开始充电");
+        } else if (voltage5.output5V() > 5) {
+            System.out.println("电压超过5V，都闪开 我要变成note7了");
+        }
+    }
+}
---
+++ KnowledgePointShareProject/knowledgepointsharelib/src/main/java/com/example/knowledgepointsharelib/adapter/sample2/classadapter/Voltage5.java
@@ -0,0 +1,9 @@
+package com.example.knowledgepointsharelib.adapter.sample2.classadapter;
+
+/**
+ * Created by sunjie on 2018/5/31.
+ * dst接口：客户需要的5V电压
+ */
+public interface Voltage5 {
+    int output5V();
+}
