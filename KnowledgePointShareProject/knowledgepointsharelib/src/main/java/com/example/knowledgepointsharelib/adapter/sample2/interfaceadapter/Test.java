5782b6caa71c789f2b4ca501d11fca324c5e7a5b
sunjie <sunjie@rgbvr.com>
2018-06-01 15:00:00 +0800

    添加对象适配器和接口适配器sample

---
+++ KnowledgePointShareProject/knowledgepointsharelib/src/main/java/com/example/knowledgepointsharelib/adapter/sample2/interfaceadapter/Test.java
@@ -0,0 +1,30 @@
+package com.example.knowledgepointsharelib.adapter.sample2.interfaceadapter;
+
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+
+/**
+ * Created by sunjie on 2018/6/1.
+ */
+
+public class Test {
+    private void test(){
+        ScaleAnimation animation = new ScaleAnimation(null, null);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
---
+++ KnowledgePointShareProject/knowledgepointsharelib/src/main/java/com/example/knowledgepointsharelib/adapter/sample2/objectadapter/Test.java
@@ -0,0 +1,26 @@
+package com.example.knowledgepointsharelib.adapter.sample2.objectadapter;
+
+import android.view.animation.ScaleAnimation;
+
+/**
+ * Created by sunjie on 2018/5/31.
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("===============对象适配器==============");
+        VoltageAdapter adapter = new VoltageAdapter(new Voltage220());
+        if (adapter != null) {
+            System.out.println("插上充电器充电");
+            if (adapter.output5V() == 5) {
+                System.out.println("电压刚刚好5V，开始充电");
+            } else {
+                System.out.println("电压不是5V，都闪开 我要变成note7了");
+            }
+        }
+        ScaleAnimation animation = new ScaleAnimation(null, null);
+        if (animation!=null) {
+
+        }
+    }
+}
