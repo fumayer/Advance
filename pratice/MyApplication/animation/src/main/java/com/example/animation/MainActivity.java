693e8e4ef1dd2e950bbd17182599f9cfc38e1400
SunJie <364793572@qq.com>
2018-12-17 21:16:44 +0800

    贝塞尔点赞飘心Activity初始化

--- pratice/MyApplication/animation/src/main/java/com/example/animation/MainActivity.java
+++ pratice/MyApplication/animation/src/main/java/com/example/animation/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.animation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void go1(View view) {
+        Toast.makeText(this, "触发go1", Toast.LENGTH_SHORT).show();
+    }
+
+    public void go2(View view) {
+        Toast.makeText(this, "触发go2", Toast.LENGTH_SHORT).show();
+    }
+
+    public void go3(View view) {
+        Toast.makeText(this, "触发go3", Toast.LENGTH_SHORT).show();
+    }
+
+    public void go4(View view) {
+        Toast.makeText(this, "触发go4", Toast.LENGTH_SHORT).show();
     }
 }
