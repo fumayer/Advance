2bb861b3b561243775c930865ded129bf4b05cff
SunJie <364793572@qq.com>
2018-12-17 21:17:21 +0800

    删除testndk

--- pratice/MyApplication/animation/src/main/java/com/example/animation/bezier/BezierActivity.java
+++ pratice/MyApplication/animation/src/main/java/com/example/animation/bezier/BezierActivity.java
@@ -3,6 +3,8 @@ package com.example.animation.bezier;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.animation.R;
+
 public class BezierActivity extends AppCompatActivity {
 
     @Override
